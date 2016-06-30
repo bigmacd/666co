5148def593fa07e964674cc34b65958d07737dba
bigmacd <martin.cooley@gmail.com>
2016-06-29 23:23:24 -0400

    more messages

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 var Botkit = require('botkit');
-var https = require('https');
 
 var ciscomessages = [ 'Hate is a strong word, but in this case not strong enough.',
-                      'Reminds me of an ugly hairless chupacabra, dead and rotting.'];
+                      'Reminds me of an ugly hairless chupacabra, dead and rotting.',
+                      "Ever squeeze those glands around a dog's ass? Yep, that green stuff that squirts out.",
+                      "Someone is having delusions of adequacy.",
+                      "I'd like to say I am happy you’re here. I’d like to."];
 
 var cbqosmessages = [ "Mike Rowe's 'Dirty Jobs' hasn't seen anything like this.",
-                      'Natural childbirth, of quadruplets, was a breeze in comparison.'];
+                      'Natural childbirth, of quadruplets, was a breeze in comparison.',
+                      'It is like every time you look down the hole of a port-a-potty.',
+                      "I didn't attend the funeral, but I sent a nice letter saying I approved of it.",
+                      'How can I explain this to you.  Let me get some crayons.'];
 
 var qsmessages = [ "Remember that scene from 'Apocolypse Now', yeah...",
-                   'The slime on the dog food factory floor?  Right under that.'];
+                   'The slime on the dog food factory floor?  Right under that.',
+                   'Sister Ursula, 4th grade elderly nun/teacher, naked.',
+                   'Some cause happiness wherever they go; others whenever they go.'];
 
 
 var controller = Botkit.slackbot();
 
-controller.hears('',["direct_mention", "mention", "direct_message", "ambient"],function(bot,message) {
+controller.hears('',["mention", "ambient"],function(bot,message) {
   // do something to respond to message
   // all of the fields available in a normal Slack message object are available
   // https://api.slack.com/events/message
@@ -31,6 +38,9 @@ controller.hears('',["direct_mention", "mention", "direct_message", "ambient"],f
   bot.reply(message, response);
 });
 
+controller.hears('',["direct_mention", "direct_message"],function(bot,message) {
+    bot.reply(message, "What? I didn't say anything!");
+});
 
 var bot = controller.spawn({
   token: require('./config').token
