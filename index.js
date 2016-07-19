9c9bb80fb8387b57601d04cd3c38afb0db3e15f3
mcooley <mcooley@sciencelogic.com>
2016-07-19 14:16:07 -0400

    fixed encoding, sort of, and consolidated messages

--- index.js
+++ index.js
@@ -4,18 +4,16 @@ var ciscomessages = [ 'Hate is a strong word, but in this case not strong enough
                       'Reminds me of an ugly hairless chupacabra, dead and rotting.',
                       "Ever squeeze those glands around a dog's ass? Yep, that green stuff that squirts out.",
                       "Someone is having delusions of adequacy.",
-                      "I'd like to say I am happy you’re here. I’d like to."];
-
-var cbqosmessages = [ "Mike Rowe's 'Dirty Jobs' hasn't seen anything like this.",
+                      "I'd like to say I am happy you're here. I'd like to.",
+                      "Mike Rowe's 'Dirty Jobs' hasn't seen anything like this.",
                       'Natural childbirth, of quadruplets, was a breeze in comparison.',
                       'It is like every time you look down the hole of a port-a-potty.',
                       "I didn't attend the funeral, but I sent a nice letter saying I approved of it.",
-                      'How can I explain this to you.  Let me get some crayons.'];
-
-var qsmessages = [ "Remember that scene from 'Apocolypse Now', yeah...",
-                   'The slime on the dog food factory floor?  Right under that.',
-                   'Sister Ursula, 4th grade elderly nun/teacher, naked.',
-                   'Some cause happiness wherever they go; others whenever they go.'];
+                      'How can I explain this to you.  Let me get some crayons.',
+                      "Remember that scene from 'Apocolypse Now', yeah...",
+                      'The slime on the dog food factory floor?  Right under that.',
+                      'Sister Ursula, 4th grade elderly nun/teacher, naked.',
+                      'Some cause happiness wherever they go; others whenever they go.'];
 
 
 var controller = Botkit.slackbot();
@@ -26,16 +24,12 @@ controller.hears('',["mention", "ambient"],function(bot,message) {
   // https://api.slack.com/events/message
   var wasSaid = message['text'];
   var response = '';
-  if (/\b[c|C]isco\b/.test(wasSaid)) {
-    response = ciscomessages[Math.floor((Math.random() * ciscomessages.length))];
+  if (/\b[c|C]isco\b/.test(wasSaid)) ||
+     (/\b[c|c][b|B][q|Q][o|O][s|S]\b/.test(wasSaid)) ||
+     (/\b[q|Q]uick[s|S]ilver\b/.test(wasSaid)) {
+        response = ciscomessages[Math.floor((Math.random() * ciscomessages.length))];
+        bot.reply(message, response);
   }
-  else if (/\b[c|c][b|B][q|Q][o|O][s|S]\b/.test(wasSaid)) {
-    response = cbqosmessages[Math.floor((Math.random() * cbqosmessages.length))];
-  }
-  else if (/\b[q|Q]uick[s|S]ilver\b/.test(wasSaid)) {
-    response = qsmessages[Math.floor((Math.random() * qsmessages.length))];
-  }
-  bot.reply(message, response);
 });
 
 controller.hears('',["direct_mention", "direct_message"],function(bot,message) {
